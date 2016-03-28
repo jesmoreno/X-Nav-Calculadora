777047c7e4620fbb8a8aef89b0e8ad391fc11e05
Jesus <jesusbasket8@gmail.com>
2016-03-29 00:43:00 +0200

    Mejorado el borrado de caracteres

--- javascript/operations.js
+++ javascript/operations.js
@@ -5,6 +5,7 @@ var nextOperand = false;
 var KEY_CODES = {13:"=",32:"RESET",46:"C",48:"0",49:"1",50:"2",51:"3",52:"4",53:"5",54:"6",55:"7",56:"8",57:"9",106:"*",107:"+",109:"-",111:"/"};
 var finalResult = "0";
 var lastCharacter = "";
+var lastCharacters = [];
 //Espacio = borrar, enter = resultado, suprimir = borrar
 
 
@@ -15,13 +16,6 @@ animationOFF = function(){
 animationON = function(){
 	document.getElementById("cover").style.animationPlayState="running";
 };
-
-function valid(){
-	if((numbers[0]&&numbers[numbers.length-1]) !== "+" && (numbers[0]&&numbers[numbers.length-1]) !== "-" && (numbers[0]&&numbers[numbers.length-1]) !== "*" && (numbers[0]&&numbers[numbers.length-1]) !== "/"){
-		return true;
-	}
-};
-
 	
 calculate = function (operand){
 	//console.log(operand);
@@ -29,39 +23,37 @@ calculate = function (operand){
 	if(operand !== "Mover tapa" && operand !== "Parar" && operand !== "RESET"){
 
 		if(operand !== "=" && operand !== "C"){
-			lastCharacter = operand;
+			lastCharacters.push(operand);
+			lastCharacter = lastCharacters[lastCharacters.length-1];
 		}
 
 		if(operand === "="){
-			if(!valid()){
-				document.getElementById("result").innerHTML="Error";
-			}else{
-				
-				switch(operations[0]){
-					case "+":
-						finalResult = parseInt(numbers[0])+parseInt(numbers[1]);
-						break;
-					case "-":
-						finalResult = parseInt(numbers[0])-parseInt(numbers[1]);
-						break;
-					case "*":
-						finalResult = parseInt(numbers[0])*parseInt(numbers[1]);
-						break;
-					case "/":
-						finalResult = (parseInt(numbers[0])/parseInt(numbers[1])).toFixed(3);
-						break;
-					default:
-						break;
-				}
-				document.getElementById("result").innerHTML=finalResult;
-				//vacio arrays de numeros y operaciones
-				console.log("resultado: "+finalResult);
-				numbers.splice(0,numbers.length);
-				operations.splice(0,operations.length);
-				//dejo en la primera posicion el resultado final
-				numbers.push(finalResult.toString());
-				lastCharacter = ""; 
+
+			switch(operations[0]){
+				case "+":
+					finalResult = parseInt(numbers[0])+parseInt(numbers[1]);
+					break;
+				case "-":
+					finalResult = parseInt(numbers[0])-parseInt(numbers[1]);
+					break;
+				case "*":
+					finalResult = parseInt(numbers[0])*parseInt(numbers[1]);
+					break;
+				case "/":
+					finalResult = (parseInt(numbers[0])/parseInt(numbers[1])).toFixed(3);
+					break;
+				default:
+					break;
 			}
+			document.getElementById("result").innerHTML=finalResult;
+			//vacio arrays de numeros y operaciones
+			console.log("resultado: "+finalResult);
+			numbers.splice(0,numbers.length);
+			operations.splice(0,operations.length);
+			//dejo en la primera posicion el resultado final
+			numbers.push(finalResult.toString());
+			lastCharacter = ""; 
+		
 
 		}else if(operand === "C"){
 
@@ -72,30 +64,40 @@ calculate = function (operand){
 				var charArray = lastNumber.split("");
 
 				if(operations[0] === lastCharacter){
-					//console.log("Es igual el operador");
 					operations.splice(0,operations.length);
-					document.getElementById("result").innerHTML = "_";
+					document.getElementById("result").innerHTML = numbers[0];
 				}else if(charArray[charArray.length-1] === lastCharacter){
+
 					console.log("Es igual el numero");
-					if(charArray.length === 1){//si solo hay uno lo elimino del array
-						//console.log("Solo hay un numero");
-						numbers.splice(0,numbers.length);
-						document.getElementById("result").innerHTML = "_";
+					if(charArray.length === 1){
+
+						if(numbers.length === 2){//elimino el numero 2 y el ultimo caracter será la operacion (+,-,* ó /)
+ 							numbers.splice(1,1);
+							document.getElementById("result").innerHTML = operations[0];
+						}else{//elimino el numero 1
+							numbers.splice(0,1);
+							document.getElementById("result").innerHTML = "0";
+						}
+
 					}else{//si el numero es de mas de una cifra extraigo solo ese
-						//console.log("cojo los numeros: "+numbers[numbers.length-1].substr(0,charArray.length-1));
+
 						numbers[numbers.length-1] = numbers[numbers.length-1].substr(0,charArray.length-1);
 						document.getElementById("result").innerHTML = numbers[numbers.length-1];
 					}
 				}
-				
-				lastCharacter = "";
-			}
-		}else{
 
-			if(numbers.length<1){
-				//console.log("inserto cuando hay 0");
-				document.getElementById("result").innerHTML=operand;
-				numbers.push(operand);
+				lastCharacters.splice(lastCharacters.length-1,1);//elimino el ultimo caracter de las lista
+				if(lastCharacters){
+					lastCharacter = lastCharacters[lastCharacters.length-1];//el ultimo caracter pasa a ser el anterior al eliminado
+				}
+			}
+		}else{// si pulsan (+,-,*,/ ó un numero)
+			console.log("a");
+			if(numbers.length<1){//para insertar un primer numero negativo
+				
+				if(operand !== "*" || operand !== "/")
+					document.getElementById("result").innerHTML=operand;
+					numbers.push(operand);
 			}else{
 				//console.log("hay mas de 1");
 				if( operand === "+" || operand === "-" || operand === "*" || operand === "/"){
